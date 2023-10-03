e6727559b9354dff4accdd26310966a0e62ad577
Florian <herrick.florian.29@gmail.com>
2023-10-03 12:52:49 +0200

    Fix login

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,39 +1,42 @@
 'use strict';
 
-window.addEventListener("load", function () {
-  document.getElementById("submit").addEventListener("click", function () {
-  let email = document.getElementById("email");
-  let password = document.getElementById("password");
-  let erreurMessage = document.querySelector(".erreurMessage");
-  fetch('http://localhost:5678/api/users/login', {
-      method: 'POST',
-      headers: { 
-          'accept': 'application/json',
-          'Content-Type': 'application/json'
-      },
+// ********************* CONSTANTS *********************
 
-      body: JSON.stringify({
-          'email': email.value,
-          'password': password.value,
-      })
-})
-      .then(response => { 
-        if (response.ok) {
-          return response.json(); 
-        } 
-        else {
-          throw new Error(response.statusText);
-        }
-      })
-      .then(data => {
-        sessionStorage.setItem("token", data.token);
-        window.location.href = "/FrontEnd/index.html";
-      })
-      .catch(error => {
-        email.style.border = '1px solid red';
-        password.style.border = '1px solid red';
-        erreurMessage.style.display = 'block';
-        console.error(error);
-      });
-  });
-})
+const emailElt         = document.getElementById("email");
+const passwordElt      = document.getElementById("password");
+const submitButtonElt  = document.getElementById("submit");
+const erreurMessageElt = document.querySelector(".error");
+
+// ********************* FUNCTIONS *********************
+
+async function login() {
+  let email    = emailElt.value;
+  let password = passwordElt.value;
+
+  const URL     = "http://localhost:5678/api/users/login";
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      'email': email,
+      'password': password,
+    })
+  };
+
+  const response = await fetch(URL, options);
+
+  if (response.ok) {
+    let data = await response.json();
+    localStorage.setItem("token", data.token);
+    window.location.href = "../index.html";
+
+  } else {
+    emailElt.style.border = '1px solid red';
+    passwordElt.style.border = '1px solid red';
+    erreurMessageElt.style.display = 'block';
+  }
+}
+
+// ********************* MAIN *********************
+
+submitButtonElt.addEventListener("click", login);
