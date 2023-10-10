9040d2adc77f880a221286ba32cdd8d2770fe817
Florian <herrick.florian.29@gmail.com>
2023-10-10 11:59:10 +0200

    Add filters

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -29,13 +29,96 @@ gallery.appendChild(figure);/Permet d'ajouter le contenue figure dans la balise 
 
 window.addEventListener("load", function () {
   let loginButton = document.getElementById("login");
-  let boutonModifier = document.querySelector('.boutonModifier');
   if (sessionStorage.getItem("token")) {
     loginButton.textContent = "logout";
-    boutonModifier.style.display = "flex";
-  } 
+  }
   else {
     loginButton.textContent = "login";
-    boutonModifier.style.display = "none";
   }
+  loginButton.addEventListener("click", function () {
+    /Vérifier si un token est présent dans le sessionStorage/
+    if (sessionStorage.getItem("token")) {
+      /Effacer le token du sessionStorage/
+      sessionStorage.removeItem("token");
+      loginButton.textContent = "login";
+
+      if (!isPageRefreshed) {
+        isPageRefreshed = true;
+        location.reload();
+      }
+    } else {
+      window.location.href = "./login.html";
+    }
+  });
 })
+
+function createfiltres(data) {
+  let gallery = document.querySelector('.gallery');
+  let firstButton = gallery.firstElementChild;
+  let allButton = document.createElement('button');
+  allButton.textContent = 'Tous';
+  allButton.classList.add("filtre");
+  gallery.insertBefore(allButton, firstButton);
+
+  allButton.addEventListener('click', () => {
+    deleteworks();
+    addAllWorks();
+  });
+
+  data.forEach(element => {
+    let name = document.createElement('button');
+    name.textContent = element.name;
+    name.classList.add("filtre");
+    gallery.appendChild(name);
+    name.addEventListener('click', () => {
+      deleteworks();
+      addworks(element.name);
+    });
+  });
+}
+
+function getWorks() {
+  return fetch('http://localhost:5678/api/works')
+    .then(response => {/renvoie toute la réponse de l'API/
+      return response.json()
+    })
+}
+
+fetch('http://localhost:5678/api/categories')
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+
+    let gallery = document.querySelector('.gallery');
+    let firstButton = gallery.firstElementChild;
+    if (sessionStorage.getItem("token") == null) {
+      createfiltres(data);
+    }
+  });
+
+function addAllWorks() {
+  getWorks().then(data => {
+    data.forEach(element => {
+      createGalleryElement(element);
+    });
+  })
+}
+
+function deleteworks() {
+  let gallery = document.querySelector('.gallery')
+  let figure = gallery.querySelectorAll('figure')
+  figure.forEach(element => {
+    gallery.removeChild(element);
+  })
+}
+
+function addworks(name) {
+  getWorks().then(data => {
+    data.forEach(element => {
+      if (element.category.name == name) {
+        createGalleryElement(element);
+      }
+    })
+  })
+}
