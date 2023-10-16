468efb9dc5123c9fc114fe16d074a961fd9f1f7e
Florian <herrick.florian.29@gmail.com>
2023-10-17 01:57:34 +0200

    Update filters + fix login

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -1,5 +1,19 @@
 'use strict';
 
+// ********************* CONSTANTS *********************
+
+
+
+// ********************* VARIABLES *********************
+
+
+
+// ********************* FUNCTIONS *********************
+
+
+
+// ********************* MAIN *********************
+
 fetch('http://localhost:5678/api/works')
 .then(response => {
 if (response.ok) {
@@ -14,18 +28,20 @@ console.log(data);
 })
 
 function createGalleryElement(element) {
-let gallery = document.querySelector('.gallery')
-let figure = document.createElement('figure');/Déclare la variable figure qui crée un élément figure/
-let img = document.createElement('img');/Déclare la variable img qui crée un élément img/
+let gallery    = document.querySelector('.gallery')
+let figure     = document.createElement('figure');/Déclare la variable figure qui crée un élément figure/
+let img        = document.createElement('img');/Déclare la variable img qui crée un élément img/
 let figcaption = document.createElement('figcaption');/Déclare la variable figcaption qui crée un élément figcaption/
-img.src = element.imageUrl;/On dit que img.src = a l'url fournie par l'api/
-img.alt = element.title;/on dit que img.alt = le titre fourni par l'api/
+img.src        = element.imageUrl;/On dit que img.src = a l'url fournie par l'api/
+img.alt        = element.title;/on dit que img.alt = le titre fourni par l'api/
 figcaption.textContent = element.title;/on dit que figcaption.textContent = le titre fourni par l'api/
 
 figure.appendChild(img);/Permet d'ajouter le contenue img dans la balise figure/
 figure.appendChild(figcaption);/Permet d'ajouter le contenue figcaption dans la balise figure/
 gallery.appendChild(figure);/Permet d'ajouter le contenue figure dans la balise gallery/
 }
+
+//**filtres */
 
 window.addEventListener("load", function () {
   let loginButton = document.getElementById("login");
@@ -47,37 +63,43 @@ window.addEventListener("load", function () {
         location.reload();
       }
     } else {
-      window.location.href = "./login.html";
+      window.location.href = "assets/login.html";
     }
   });
 })
 
-function createfiltres(data) {
-  let gallery = document.querySelector('.gallery');
-  let firstButton = gallery.firstElementChild;
-  let allButton = document.createElement('button');
-  allButton.textContent = 'Tous';
-  allButton.classList.add("filtre");
-  gallery.insertBefore(allButton, firstButton);
+function createFilters(data) {
+  let filters     = document.querySelector('.filters');
+  let firstButton = filters.firstElementChild;
 
+  console.log(firstButton);
+
+  let allListItem = document.createElement('li');
+  let allButton   = document.createElement('button');
+  allButton.textContent = 'Tous';
+  filters.insertBefore(allButton, firstButton);
   allButton.addEventListener('click', () => {
-    deleteworks();
+    deleteWorks();
     addAllWorks();
   });
 
+  console.log(filters);
+
   data.forEach(element => {
-    let name = document.createElement('button');
+    let name         = document.createElement('button');
     name.textContent = element.name;
-    name.classList.add("filtre");
-    gallery.appendChild(name);
+    filters.appendChild(name);
+
+    console.log(filters);
+
     name.addEventListener('click', () => {
-      deleteworks();
-      addworks(element.name);
+      deleteWorks();
+      addWorks(element.name);
     });
   });
 }
 
-function getWorks() {
+async function getWorks() {
   return fetch('http://localhost:5678/api/works')
     .then(response => {/renvoie toute la réponse de l'API/
       return response.json()
@@ -90,10 +112,10 @@ fetch('http://localhost:5678/api/categories')
   })
   .then(data => {
 
-    let gallery = document.querySelector('.gallery');
+    let gallery     = document.querySelector('.gallery');
     let firstButton = gallery.firstElementChild;
     if (sessionStorage.getItem("token") == null) {
-      createfiltres(data);
+      createFilters(data);
     }
   });
 
@@ -105,15 +127,15 @@ function addAllWorks() {
   })
 }
 
-function deleteworks() {
+function deleteWorks() {
   let gallery = document.querySelector('.gallery')
-  let figure = gallery.querySelectorAll('figure')
+  let figure  = gallery.querySelectorAll('figure')
   figure.forEach(element => {
     gallery.removeChild(element);
   })
 }
 
-function addworks(name) {
+function addWorks(name) {
   getWorks().then(data => {
     data.forEach(element => {
       if (element.category.name == name) {
